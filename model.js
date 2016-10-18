fc258d5b122cc132ec0fad064a6511497bf13447
xiaoheiheihei <bo.nov29@gmail.com>
2016-10-18 13:42:20 -0700

    ~

--- model.js
+++ model.js
@@ -10,15 +10,37 @@ connection.connect();
 
 
 
-function Item(tableName) {
+function Record(tableName) {
   this.tableName = tableName;
 }
-Item.prototype.selectAll = function(cb) {
-  connection.query('SELECT * from ' + this.tableName, function(err, rows, fields) {
-    if (err) reject(err);
+Record.prototype.selectAll = function(cb) {
+  connection.query('SELECT * from ' + this.tableName , function(err, rows, fields) {
+    if (err)  throw err;
     cb(rows);
   });
 };
 
-module.exports.Item = Item;
+Record.prototype.findBy = function(name, cb) {
+  connection.query('SELECT * from ' + this.tableName + ' where name = ' + '\'' + name + '\'', function(err, rows, fields) {
+    if (err) throw err;
+    cb(rows);
+  });
+};
+
+Record.prototype.insert = function(dog, cb){
+  connection.query('INSERT INTO ' + this.tableName + ' SET ?', dog, function(err,res){
+    if(err) throw err;
+    cb(res);
+  });
+};
+
+Record.prototype.getScheme = function(cb){
+  connection.query( "DESCRIBE " + this.tableName, function(err, rows, fields){
+    if (err) throw err;
+    cb(rows);
+  } );
+
+}
+
+module.exports.Record = Record;
 module.exports.connection = connection;
--- test.js
+++ test.js
@@ -1,13 +1,31 @@
 var model = require("./model.js");
 
 
-var Item = model.Item;
+var Record = model.Record;
 var connection = model.connection;
 
 
 
 
-var pet = new Item('pet');
-pet.selectAll(function(result){console.log('The result is: \n', result);})
+var pet = new Record('pet');
+pet.selectAll(function(result){console.log('The result is: \n', result);});
+
+// pet.findBy('Puffball', function(result){console.log('The result is: \n', result);});
+
+var dog = {
+    name: 'SnowBall',
+    owner: 'Bo',
+    species: 'Hasky',
+    sex: 'm',
+    birth: new Date(1999, 3, 30),
+    death: null };
+
+// pet.insert(dog, function(result){console.log('The result is: \n', result);});
+
+// pet.getScheme( function(result){console.log('The result is: \n', result);} );
+
+
+
+
 
 connection.end();
